0a5482fc630adb5b0313fab304013b673d05eebc
Daniella D <danielladevoto@gmail.com>
2018-03-11 21:39:25 -0400

    working on server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+// Dependencies 
+var express = require("express");
+var expressHandlebars = require("express-handlebars");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var cheerio = require("cheerio");
+var request = require("request");
+
+// Require all models
+var db = require("./models");
+
+// Initialize Express
+var app = express();
+
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: false }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+
+// By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost/populate", {
+  useMongoClient: true
+});
