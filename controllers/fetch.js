c0cfdfe32e38893f859671dfd9f479de5df7dc9e
Daniella D <danielladevoto@gmail.com>
2018-03-17 23:48:33 -0400

    working on routes

--- controllers/fetch.js
+++ controllers/fetch.js
@@ -9,3 +9,5 @@ db.on("error", console.error.bind(console, "Connection error:"));
 db.once("open", function(callback){
   console.log("Connection Succeeded."); /* Once the database connection has succeeded, the code in db.once is executed. */
 });
+
+module.exports = db;
--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,55 @@
 var fetch = require('../controllers/fetch.js')
-var headline = require('../controllers/headline.js')
+var recipe = require('../controllers/headline.js')
 var notes = require('../controllers/note.js')
 
 module.exports = function (app) {
+// Routes
 
-  app.get('/', function(req, res){
-    let renderObject = {};
-    headline.findAll(function(data){
-      renderObject.tours = data;
-      console.log(data)
-      res.render('home', {data:data})  
+// POST route for saving a new Book to the db and associating it with a Library
+app.post("/submit", function(req, res) {
+  // Create a new Comment in the database 
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Library.findOneAndUpdate({}, { $push: {note: dbNote._id } }, { new: true });
     })
-  })
+    .then(function(dbLibrary) {
+      // If the Library was updated successfully, send it back to the client
+      res.json(dbLibrary);
+    })
+    .catch(function(err) {
+      // If an error occurs, send it back to the client
+      res.json(err);
+    });
+});
 
-  app.get('/recipes', function(req, res) {
-    notes.findAll(function(data){
-      res.json(data)
+// Route for getting all books from the db
+app.get("/", function(req, res) {
+  // Using our Recipe model, "find" every recipe in our db
+  recipe.find({})
+    .then(function(dbRecipe) {
+      // If any Recipes are found, send them to the client
+      res.json(dbRecipe);
     })
-  })
-  app.get('/scrape', function () {
-    console.log('"/scrape" get ')
-    fetch.scrapedToDb(function() {
-    })
-  })
+    .catch(function(err) {
+      // If an error occurs, send it back to the client
+      res.json(err);
+    });
+});
 
-  app.post('/newcomment/:id', function (req, res) {
-    notes.newNote({note:req.body, recipe: req.params.id} , function(data) {
-      console.log('data', data)
+// Route to see what library looks like WITH populating
+app.get("/populated", function(req, res) {
+  // Using our Note model, "find" every recipe in our db and populate them with any associated comments
+  db.Library.find({})
+    // Specify that we want to populate the retrieved recipes with any associated comments
+    .populate("notes")
+    .then(function(dbLibrary) {
+      // If any Recipes are found, send them to the client with any associated comments
+      res.json(dbLibrary);
     })
-    console.log('"/newcomment" post ')
-  })
-  
-}
+    .catch(function(err) {
+      // If an error occurs, send it back to the client
+      res.json(err);
+    })
+  });
+};
